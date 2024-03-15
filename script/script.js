07e9c43257bc362f839e6f735d756feddcadbe56
Aziz <qwerrt@gmail.com>
2024-03-15 23:37:19 +0500

    first

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelectorAll('.header__link')
+
+btn.forEach((button) => {
+    button.addEventListener('click', function () {
+        alert('Sorri mi ne rabotayem he-he')
+    })
+});
